ca956305cf0a02dc0dcf63e10e75ca367c344079
abanobmicheal406-ctrl <abanobmicheal406@gmail.com>
2026-06-25 06:21:28 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,53 @@
+// 霸都丶傲天 2019/7/9  https://Github.com/AJLoveChina
+var config = {
+    // 下面的句子不一定非要7句， 你也可以改成4句，2句话都可以
+lines: [
+ "Ich liebe dich.",
+ "Du bist der schönste Teil meines Lebens.",
+    "Ich gieße mein Herz in einen Kelch der Liebe für sie, die das Wesen meiner Seele mit Poesie, Kardamom und Safran kostet.",
+
+    "Ich gieße es für sie, an deren Ufern mein Wahnsinn versinkt, deren Wimpern mich sehnsüchtig anziehen und deren Lider mich zärtlich umschließen.",
+
+    "Ich gieße es für sie, die mich wie einen durchsichtigen Traum birgt, der die Augen eines klugen Jungen heimsucht,",
+
+    "der seit seiner Kindheit gewohnt ist, die warme Tinte auf dem Papier zu küssen, die die Reinheit des Herzens symbolisiert,",
+
+    "entzündet von einer Glut im Paradies.",
+
+    "Ich gieße es für sie, die meine Tränen wie bitteres Salz schmeckt, wenn meine Meere toben und wogen,",
+
+    "das Schiff meiner Brust überläuft vor Kummer, die Zeit sich verengt und der Raum tobt.",
+
+    "Ihr, die, wenn meine Wunde schreit und meine Blutung Spuren des Kummers in mir hinterlässt, auf mich zueilt,",
+
+    "meinen Kummer umarmt, ihr Trost meine Gedanken besänftigt und mir das Rascheln des Gartens für den Brachvogel vorsingt.",
+
+    "Ihr, deren unverfälschter Duft meine Gedanken erweckt, reise ich in ihr und sie in mir,",
+
+    "auf dem Sattel der Pferde der Gewissheit, auf einem Pfad, dessen Grün von Geborgenheit singt.",
+
+    "Ihr, die ich bisher nur auf dem tückischen Meer der Träume gefunden habe, erscheint sie vor meinen Augen wie eine Nymphe,",
+
+    "schwimmend und schwebend, erwachend und schlafend, mich zurücklassend zwischen dem Ufer der Hoffnungen",
+
+    "und der Wüste meines Lebens.",
+
+    "Mich in ihrer Liebe mit klagenden Melodien herausfordernd,",
+
+    "in denen das Stöhnen der Rabab sich in der Beichte an sie",
+
+    "und das Wehklagen der Violine über sie ergeht.",
+        "SANDOURA I LOVE YOU DEEPLY ",
+            "I'M SORRRRRYYYY"
+    
+],
+    // 相爱的时间 记住格式不能写错了, 非常重要
+    // 格式一定要和下面这个时间格式一样
+    // 分别是 年-月-日T时时:分分:秒秒
+    //      温馨警告:
+    //          1.时分秒都必须是俩个数字  比如 00:00:00
+    //          2.T不能丢了
+    date: "2023-09-12T20:15:00",
+    // 双方的名字
+    names: ["7OK4A", "7OK4"]
+}
